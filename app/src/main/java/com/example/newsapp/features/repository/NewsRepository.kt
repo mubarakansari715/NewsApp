104db6b9274b547bc192ee511486be02515633e3
mubarak <143716857+mubarakbacancy@users.noreply.github.com>
2024-12-02 16:40:56 +0530

    - Added NetworkResponse state - Added flow to response handling

--- app/src/main/java/com/example/newsapp/features/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/features/repository/NewsRepository.kt
@@ -1,12 +1,21 @@
 package com.example.newsapp.features.repository
 
+import com.example.newsapp.features.model.Article
 import com.example.newsapp.network.NewsApiService
-import com.example.newsapp.features.model.NewsResponse
+import com.example.newsapp.utils.NetworkResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class NewsRepository @Inject constructor(private val apiService: NewsApiService) {
 
-    suspend fun getAllNews(): NewsResponse {
-        return apiService.getAllNews()
+    fun getAllNews(): Flow<NetworkResponse<List<Article>>> = flow {
+        try {
+            emit(NetworkResponse.Loading)
+            val response = apiService.getAllNews()
+            emit(NetworkResponse.Success(response.articles))
+        } catch (e: Exception) {
+            emit(NetworkResponse.Error(e.toString()))
+        }
     }
 }
--- app/src/main/java/com/example/newsapp/features/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/features/viewmodel/NewsViewModel.kt
@@ -4,8 +4,10 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.newsapp.features.repository.NewsRepository
+import com.example.newsapp.utils.NetworkResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,9 +15,20 @@ class NewsViewModel @Inject constructor(private val repository: NewsRepository) 
 
 
     fun getAllNews() {
-        viewModelScope.launch {
-            val response = repository.getAllNews()
-            Log.e("TAG", "getAllNews: $response")
-        }
+        repository.getAllNews().onEach { result ->
+            when (result) {
+                is NetworkResponse.Loading -> {
+                    Log.e("TAG", "getAllNews: Loading")
+                }
+
+                is NetworkResponse.Error -> {
+                    Log.e("TAG", "getAllNews: Error")
+                }
+
+                is NetworkResponse.Success -> {
+                    Log.e("TAG", "getAllNews: Success ${result.result}")
+                }
+            }
+        }.launchIn(viewModelScope)
     }
 }
